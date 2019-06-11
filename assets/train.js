7243fe50703a710c7d28928b255d84ab7c92fb09
Paris Cornett <pariscornett@gmail.com>
2019-06-11 18:49:08 -0500

    code to dynamically add rows to table with user-input :sparkles:

--- assets/train.js
+++ assets/train.js
@@ -18,8 +18,10 @@ let destination;
 let trainTime;
 let frequency;
 
-//create an event handler to store user input in variables, and display them into the table. 
+
+//create an event handler to store user input in variables, then firebase, and then display onto the table. 
 $("#submit").on("click", function (){
+  //isolates the text entered by user and stores it in a variable
     trainName = $("#train-name-input").val().trim();
     destination = $("#destination-input").val().trim();
     trainTime = $("#train-time-input").val().trim();
@@ -30,14 +32,25 @@ $("#submit").on("click", function (){
     console.log(trainTime);
     console.log(frequency);
 
-    database.ref().set({
+  //empties the input boxes to avoid user needing to backspace
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#train-time-input").val("");
+    $("#frequency-input").val("");
+
+  //pushes variable information to the firebase database and stores it there
+    database.ref().push({
         trainName : trainName,
         destination : destination, 
         trainTime : trainTime,
-        frequency : frequency
+        frequency : frequency,
+        trainNumber : trainNumber
     })
+  
+  //variable making a new row in a table with the information from each input area
+    let addRow = " <tr><td>" + trainName + "<td>" + destination + "<td>" + frequency + "<td>" + "placeholder" + "<td>" + "placeholder" + "</tr>";
+  
+  //appending the aforementioned markup to DOM 
+    $("#display-table").append(addRow);
 })
 
-// database.ref().on("value", function(snapshot){
-//     console.log(snapshot.val())
-// })
